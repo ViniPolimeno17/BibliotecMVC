f9367e62fb51cc20637c9582e349ad008e4c9f38
Vinicius Bernardo <vivianepolimeno@gmail.com>
2024-12-12 16:27:21 -0300

    projetao do natalzao subir banco de dados

--- Bibliotec/obj/Debug/net8.0/Bibliotec.AssemblyInfo.cs
+++ Bibliotec/obj/Debug/net8.0/Bibliotec.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bibliotec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e24950290cc3b460bc7bce919dd9772229f5bfb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82bea8a82e528850567efb04e556fd3583a41264")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bibliotec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bibliotec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
